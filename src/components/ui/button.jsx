ce2bbdeecb24ee4507040e2697b9838acf4bbfbd
TaskReminder <21104023@ittelkom-pwt.ac.id>
2025-01-22 22:14:43 +0700

    update

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -20,9 +20,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:"hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        line:"border border-primary text-primary text-center hover:text-primary hover:bg-accent",
         auto: "bg-primary/80 text-primary-foreground hover:bg-primary/100 w-20",
-        line: "border border-primary text-primary w-20 text-center hover:text-primary hover:bg-accent",
+        line: "border border-primary text-primary text-center hover:text-primary hover:bg-accent",
         long: "bg-primary/80 text-primary-foreground hover:bg-primary/100 w-40",
       
       },
